eaf916f39324c6106514e8cf9af6b25f050b3bac
inu <45503605+inu1104@users.noreply.github.com>
2025-06-12 22:28:00 +0800

    Create 0073_set_matrix_zeroes.cpp

---
+++ LeetCode/0073_set_matrix_zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        set<int> reset_row;
+        set<int> reset_col;
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    reset_row.insert(i);
+                    reset_col.insert(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.size(); i++)
+            for (int j = 0; j < matrix[0].size(); j++)
+                if (reset_row.find(i) != reset_row.end() || reset_col.find(j) != reset_col.end())
+                    matrix[i][j] = 0;
+    }
+};
